0a1406c8120da4532c9d5499fcb5cea4c5e23d91
Amanda Montana Pearce <amandamontanapearce@gmail.com>
2019-04-21 22:20:35 -0600

    add html with a style tag

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return '<h1 style="color: blue">Hello, World!</h1><p>This has html</p>'
 
 if __name__ == '__main__':
     app.run()
